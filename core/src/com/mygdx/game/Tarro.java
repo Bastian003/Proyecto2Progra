d0b457348d42bc5fbf7ba386e72601620ecc57c2
bastian <bastian@DESKTOP-F2KPMRG>
2024-06-27 18:54:51 -0400

    avance parte final

---
+++ core/src/com/mygdx/game/Tarro.java
@@ -0,0 +1,93 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Tarro extends ElementoJuego {
+    private Rectangle bucket;
+    private Texture bucketImage;
+    private Sound sonidoHerido;
+    private int vidas;
+    private int puntos;
+    private int velx;
+    private boolean herido;
+    private int tiempoHeridoMax;
+    private int tiempoHerido;
+
+    public Tarro(Texture tex, Sound ss) {
+        this.bucketImage = tex;
+        this.sonidoHerido = ss;
+        this.vidas = 3;
+        this.puntos = 0;
+        this.velx = 400;
+        this.herido = false;
+        this.tiempoHeridoMax = 50;
+    }
+
+    public int getVidas() {
+        return vidas;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public Rectangle getArea() {
+        return bucket;
+    }
+
+    public void sumarPuntos(int pp) {
+        puntos += pp;
+    }
+
+    @Override
+    public void crear() {
+        bucket = new Rectangle();
+        bucket.x = 800 / 2 - 64 / 2;
+        bucket.y = 20;
+        bucket.width = 64;
+        bucket.height = 64;
+    }
+
+    @Override
+    public void dañar() {
+        vidas--;
+        herido = true;
+        tiempoHerido = tiempoHeridoMax;
+        sonidoHerido.play();
+    }
+
+    @Override
+    public void dibujar(SpriteBatch batch) {
+        if (!herido) {
+            batch.draw(bucketImage, bucket.x, bucket.y);
+        } else {
+            batch.draw(bucketImage, bucket.x, bucket.y + MathUtils.random(-5, 5));
+            tiempoHerido--;
+            if (tiempoHerido <= 0) herido = false;
+        }
+    }
+
+    @Override
+    public void actualizarMovimiento() {
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) bucket.x -= velx * Gdx.graphics.getDeltaTime();
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += velx * Gdx.graphics.getDeltaTime();
+        if (bucket.x < 0) bucket.x = 0;
+        if (bucket.x > 800 - 64) bucket.x = 800 - 64;
+    }
+
+    @Override
+    public void destruir() {
+        bucketImage.dispose();
+    }
+
+    public boolean estaHerido() {
+        return herido;
+    }
+}
